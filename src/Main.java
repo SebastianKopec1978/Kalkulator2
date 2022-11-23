9ac92a214b90fa39017d839aaf1b520d117465db
Your Name <you@example.com>
2022-11-23 20:57:04 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Witaj w kalkulatorze \n Podaj pierwszą liczbę");
+        double liczba1=sc.nextDouble();
+        System.out.println("Podaj drugą liczbę");
+        double liczba2=sc.nextDouble();
+        System.out.println("1.dodawanie, 2.odejmowanie,3.mnożenie, 4.dzielenie");
+        System.out.println("Jaką operację chcesz wykonać?");
+        int opcja=sc.nextInt();
+        switch(opcja){
+            case 1: System.out.println(liczba1+liczba2);
+            break;
+            case 2: System.out.println(liczba1-liczba2);
+            break;
+            case 3: System.out.println(liczba1*liczba2);
+            break;
+            case 4:   System.out.println(liczba1/liczba2);
+            break;
+
+        }
+
+
+
+
+    }
+}
